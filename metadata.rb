009cb6f6ed857295b088c56f15dbde14f86d98a1
Ker Ruben Ramos <xdiscent@gmail.com>
2013-10-11 04:02:45 +0800

    Bump
    * Update metadata
    * Add Berksfile

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer_email "conrad@kramerapps.com"
 license          "Apache 2.0"
 description      "Deploys and configures Node.js applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.1.1"
 
 depends          "nodejs"
-depends          "application"
+depends          "application", "= 2.0.4"
 
 suggests         "strongloop"
 
